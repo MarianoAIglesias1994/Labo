0abdf8c3b7d4588a8e20cb35ef66e6f3a5eb5920
Mariano Agustín Iglesias <marianoiglesias1994@hotmail.com>
2016-11-13 20:38:20 -0300

    Interrupciones andan, I2C no
    Andan las interrupciones por separado, el problema estaba en los V.I.
    con los .org ... Anda el modo bajo consumo chequeado con
    amperimetro...Problema rutinas I2C no se cambia el valor de TWINT

--- Test081116/081116/081116/main.asm
+++ Test081116/081116/081116/main.asm
@@ -14,17 +14,14 @@
 
 ***************************************************************/
 
-
 .cseg
 .org 0x00
 rjmp INICIO
-;ver ubicación de INT0 en Vector de Interrupciones Atmel
-.org 0x02
+;.org 0x02
 rjmp ISR_INT0_INACTIVITY	
-;Idem INT1
-.org 0x04
+;.org 0x04
 rjmp ISR_INT1_BLUETOOTH	
-.org 0x3E
+
 
 INICIO:
 
@@ -33,37 +30,45 @@ INICIO:
 	LDI R21, LOW(RAMEND)
 	OUT SPL, R21
 
+	
+LDI R20, 0b00001000
+OUT DDRC, R20	;
+LDI R20, 0b00001000
+OUT PORTC, R20	;
+
+LDI R20, 0b00010000
+OUT DDRD, R20	;
+LDI R20, 0b00010000
+OUT PORTD, R20	;
+
 /**************************************************************
 CONFIGURACION INTERRUPCIONES EXTERNAS
 ***************************************************************/
 CONFIG_INT:
-	cli
-	sei
+
+	;EICRA si se configura por flanco o por nivel, hablarlo, depende de la interrupcion INT0 o INT1 utilizada en la seccion de arriba
+	;out R18, EICRA      ;me fijo que hay en eicra
+	ldi R16,0b00000101		;ldi R16,0b00000001 para INT0;;;;; ldi R16,0b00000101 para INT0 e INT1 
+	sts EICRA, R16		;habilito todo despues lo cambio
 
 	;EIMSK
 	;out R18, EIMSK      ;me fijo que hay en eimsk
 	ldi R16,0b00000011 		; 	ldi R16,0b00000001 para INT0;;;;; ldi R16,0b00000011 para INT0 e INT1 
 	out EIMSK,R16		;habilito las dos interrupciones externas del mcu. INT0 e INT1
 
-	
-	;EICRA si se configura por flanco o por nivel, hablarlo, depende de la interrupcion INT0 o INT1 utilizada en la seccion de arriba
-	;out R18, EICRA      ;me fijo que hay en eicra
-	ldi R16,0b00000101		;ldi R16,0b00000001 para INT0;;;;; ldi R16,0b00000101 para INT0 e INT1 
-	sts EICRA, R16		;habilito todo despues lo cambio
-
-
 	;EIFR tiene que estar seteado junto con el bit de interrupcion globar para que al momento de suceder la interrupcion 
 	;in R18, EIFR      ;me fijo que hay en eifr
 	ldi R16,0b00000011		; 	ldi R16,0b00000001 para INT0;;;;; ldi R16,0b00000011 para INT0 e INT1 
 	out EIFR,R16		;habilito todo despues lo cambio
-		
+
+	SEI
 /**************************************************************
 CONFIGURACION BAJO CONSUMO
 ***************************************************************/
 	;por ahora lo comento, despues probamos
 CONFIG_BAJO_CONSUMO:
 	;configuro el consumo de energia, PSM o PDM
-	ldi R16,0x05	;0000 0101 ;;;; 010 es de power down mode y el lsb es para activar el sleep mode
+	ldi R16,0b00000100	;0000 0101 ;;;; 010 es de power down mode y el lsb es para activar el sleep mode
 	out SMCR,R16
 
 	
@@ -74,13 +79,19 @@ INCIALIZO I2C
 RCALL I2C_INIT
 
 
-
-
 /**************************************************************
 CONFIGURACION ACELEROMETRO
 ***************************************************************/
-/*CONFIG_ACELEROMETRO:
+CONFIG_ACELEROMETRO:
 ;Información sacada de la data sheet del ADXL345
+;Arduino Pin	ADXL345 Pin		Placa CdR 88papu
+;GND				GND				8,22
+;3V3				VCC				7,20
+;3V3				CS				
+;GND				SDO				
+;A4					SDA				27
+;A5					SCL				28
+;					INT1			4
 
 	LDI R24, 0b10100110	;Dirección del esclavo SLA(1010011) + Write(0)
 	LDI R25, 0b00011001	;Dirección del registro a escribir ;THRESH_INACT 0x25 0b00011001
@@ -88,6 +99,8 @@ CONFIGURACION ACELEROMETRO
 				;en 128 despues hacer ajuste fino
 	RCALL SINGLE_BYTE_WRITE
 
+	
+	
 	LDI R25, 0b00011010	;Dirección del registro a escribir ;TIME_INACT 0x26 0b00011010 umbral de tiempo de inactividad
 	LDI R26, 0b00001010	;Dato a transmitir ;TIME_INACT  (máximo 255 segundos) (1 sec/LSB)
 				;en 10s despues hacer ajuste fino
@@ -117,7 +130,7 @@ CONFIGURACION ACELEROMETRO
 ;Sólo queda la duda de si en Reg 0x2D POWER_CTL hay que poner el (Measure)D3 en 1 para que anden las interrupciones o no. Por defecto está en 0
 
 
-*/
+
 
 /**************************************************************
 CONFIGURACION RTC
@@ -133,17 +146,72 @@ CONFIGURACION BLUETOOTH
 /**************************************************************
 BLOQUE PRINCIPAL
 ***************************************************************/
+LDI R20, 0b00001000
+OUT DDRC, R20	;
+LDI R20, 0b00001000
+OUT PORTC, R20	;
+
+LDI R20, 0b00010000
+OUT DDRD, R20	;
+LDI R20, 0b00010000
+OUT PORTD, R20	;
 
 ;se queda esperando que haya una interrupcion 
 SLEEP_MODE:
+			ldi R16,0b00000101	;0000 0101 ;;;; 010 es de power down mode y el lsb es para activar el sleep mode
+			out SMCR,R16
 			SLEEP
+			ldi R16,0b00000100	;0000 0100 ;;;; 010 es de power down mode y el lsb es para activar el sleep mode
+			out SMCR,R16
+			
 			CPI R19,1
-			BREQ ALARMA
+			BREQ ALARMA;_WRAPPER
 RETORNO_ALARMA:
 			CPI R19,2
-			BREQ BLUETOOTH
+			BREQ BLUETOOTH;_WRAPPER
 RETORNO_BLUETOOTH:
 			RJMP SLEEP_MODE
+
+/*
+ALARMA_WRAPPER:
+RCALL ALARMA
+RJMP SLEEP_MODE
+
+BLUETOOTH_WRAPPER:
+RCALL BLUETOOTH
+RJMP SLEEP_MODE
+*/
+/**************************************************************
+RUTINA ENVIO DE ENVIO DE DATOS POR BLUETOOTH 
+***************************************************************/
+BLUETOOTH:
+LDI R21, 10	;valor a ajustar tiempo 
+
+PARPADEO2:
+		LDI R20, 0b00000000
+		OUT PORTD, R20	
+		RCALL RETARDO2
+		LDI R20, 0b00010000
+		OUT PORTD, R20	
+		RCALL RETARDO2
+		DEC R21
+		CPI R21, 0
+		BRNE PARPADEO2
+		;ver si falta algo más
+		LDI R19,0
+		RJMP RETORNO_BLUETOOTH
+
+RETARDO2:
+		LDI R16, 10
+LOOP12:	LDI R17, 255
+LOOP22:	LDI R18, 255
+LOOP32:  DEC R18
+		BRNE LOOP32			
+		DEC R17
+		BRNE LOOP22
+		DEC R16
+		BRNE LOOP12
+		RET
 
 
 /**************************************************************
@@ -151,22 +219,20 @@ RUTINA DE ALARMA
 ***************************************************************/
 ALARMA:
 /*PRENDE Y APAGA LED (EQUIVALENTE A SONAR ALARMA) Y GAURDAR DATOS EN RTC*/
-LDI R20, 0b00001000
-OUT DDRC, R20	;el port C esta como salida
 LDI R21, 10	;valor a ajustar tiempo 
 
 PARPADEO:
 		LDI R20, 0b00000000
-		OUT PORTC, R20	;los pines de port C estan en 0
+		OUT PORTC, R20	
 		RCALL RETARDO
 		LDI R20, 0b00001000
-		OUT PORTC, R20	;los pines de port C estan en 1
+		OUT PORTC, R20	
 		RCALL RETARDO
 		DEC R21
 		CPI R21, 0
 		BRNE PARPADEO
 		;ver si falta algo más
-		;LDI R19,0
+		LDI R19,0
 		RJMP RETORNO_ALARMA
 
 RETARDO:
@@ -182,54 +248,20 @@ LOOP3:  DEC R18
 		RET
 
 
-/**************************************************************
-RUTINA ENVIO DE ENVIO DE DATOS POR BLUETOOTH 
-***************************************************************/
-BLUETOOTH:
-LDI R20, 0b00010000
-OUT DDRD, R20	;el port C esta como salida
-LDI R21, 10	;valor a ajustar tiempo 
 
-PARPADEO2:
-		LDI R20, 0b00000000
-		OUT PORTD, R20	;los pines de port C estan en 0
-		RCALL RETARDO2
-		LDI R20, 0b00010000
-		OUT PORTD, R20	;los pines de port C estan en 1
-		RCALL RETARDO2
-		DEC R21
-		CPI R21, 0
-		BRNE PARPADEO2
-		;ver si falta algo más
-		;LDI R19,0
-		RJMP RETORNO_BLUETOOTH
-
-
-RETARDO2:
-		LDI R16, 10
-LOOP12:	LDI R17, 255
-LOOP22:	LDI R18, 255
-LOOP32:  DEC R18
-		BRNE LOOP32			
-		DEC R17
-		BRNE LOOP22
-		DEC R16
-		BRNE LOOP12
-		RET
-
-
-;RJMP RETORNO_BLUETOOTH
 
 /**************************************************************
 RUTINA DE SERVICIO DE INTERRUPCION POR INACTIVIDAD DEL ACELEROMETRO
 ***************************************************************/
 ISR_INT0_INACTIVITY:
-PUSH R16; se guarda para que no se pise lo que habia en r16 antes de la interrupcion
+CLI
+/*PUSH R16
 IN R16, SREG
 PUSH R16
 POP R16
 OUT SREG, R16
-POP R16
+POP R16*/
+SEI
 LDI R19,1	
 RETI
 
@@ -238,12 +270,14 @@ RETI
 RUTINA DE SERVICIO DE INTERRUPCION POR ACTIVACION DEL BLUETOOTH
 ***************************************************************/
 ISR_INT1_BLUETOOTH:
-PUSH R16; se guarda para que no se pise lo que habia en r16 antes de la interrupcion
+CLI
+/*PUSH R16
 IN R16, SREG
 PUSH R16
 POP R16
 OUT SREG, R16
-POP R16
+POP R16*/
+SEI
 LDI R19,2	
 RETI
 
@@ -264,7 +298,7 @@ CONFIG_I2C:
 I2C_INIT:
 	LDI R21, 0		
 	STS TWSR, R21		;Preescaler 1 en TWI Status Reg
-	LDI R21, 0x47
+	LDI R21, 0b11111111		;0x47
 	STS TWBR, R21		;Setea la frecuencia a 50k (8MHz XTAL)
 	LDI R21, (1<<TWEN)	;0x04 a R21 (TWEN: Enable bit)
 	STS TWCR, R21		;Habilita el TWI 
@@ -278,11 +312,12 @@ I2C_INIT:
 I2C_START:
 	LDI R21, (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
 	STS TWCR, R21		;Transimitir condición de START
-
+	
 WAIT1:
 	LDS R21, TWCR		;Lee el registro
 	SBRS R21, TWINT		;Saltea siguiente linea si TWINT es 1(==operación finalizada)
 	RJMP WAIT1		;TWINT está en 0
+	RJMP BLUETOOTH	;problema nada está haciendo que TWINT se ponga en 1
 	RET
 
 
@@ -311,8 +346,10 @@ I2C_STOP:
 
 SINGLE_BYTE_WRITE:
 	RCALL I2C_START		;Transmite la condición de START
+	
 	MOV R27, R24		;Carga la dirección del esclavo + configuración R/W
 	RCALL I2C_WRITE		;Escribe R27 al bus I2C
+
 	MOV R27, R25		;Dirección del registro a escribir
 	RCALL I2C_WRITE		;Escribe R27 al bus I2C
 	MOV R27, R26		;Dato a transmitir 
