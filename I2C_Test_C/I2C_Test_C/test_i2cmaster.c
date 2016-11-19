9183fdb43ce05a661eac7768a6793dc89130b9d8
Mariano Agustín Iglesias <marianoiglesias1994@hotmail.com>
2016-11-18 21:17:17 -0300

    Cambios

--- I2C_Test_C/I2C_Test_C/test_i2cmaster.c
+++ I2C_Test_C/I2C_Test_C/test_i2cmaster.c
@@ -22,7 +22,6 @@ Description:
 int main(void)
 {
     unsigned char ret;
-    
 
     DDRC  = 0x0C;                              // LEDs Rojos 
     PORTC = 0x0C;                              // (active low LED's )
@@ -78,7 +77,89 @@ CONTINUE:
         
         if(ret == 0xE5)
             PORTC = 0x00;                      // Prende LEDs Rojos en caso de Lectura correcta
-    }
+    
+	//
+	 i2c_start_wait(DevADXL345+I2C_WRITE);     // set device address and write mode
+	 i2c_write(0x25);                        // write address = 5
+	 i2c_write(0b00010001);
+	 i2c_stop();
+	 
+	 
+	 i2c_start_wait(DevADXL345+I2C_WRITE);     // set device address and write mode
+	 i2c_write(0x25);                        // write address = 5
+	i2c_rep_start(DevADXL345+I2C_READ);       // set device address and read mode
+	ret = i2c_readNak();                    // read one byte
+	i2c_stop();
+	
+	if(ret == 0xE5)
+	PORTC = 0x00;                      // Prende LEDs Rojos en caso de Lectura correcta
+	//
+	
+	
+	
+	
+	/*
+
+; Se comienza la secuencia de seteo del acelerometro en los valores necesarios
+
+	LDI R25, 0x25		;Direccion del registro a escribir ;THRESH_INACT 0x25 0b0010010
+	LDI R26, 0b00010001	;Dato a transmitir ;THRESH_INACT  8 bit unsigned. No dejar en 0x00 umbral de inactividad (62.5 mg/LSB) 
+				;en 17 = 0b00010001 despues hacer ajuste fino
+	RCALL MULTIPLE_BYTE_WRITE
+;	RCALL DELAY
+
+
+	LDI R25, 0x26		;Direccion del registro a escribir ;TIME_INACT 0x26 0b00100110 umbral de tiempo de inactividad
+	LDI R26, 0b00000001	;Dato a transmitir ;TIME_INACT  (maximo 255 segundos) (1 sec/LSB)
+				;en 1s despues hacer ajuste fino
+	RCALL MULTIPLE_BYTE_WRITE
+;	RCALL DELAY
+
+
+	LDI R25, 0x27		;Direccion del registro a escribir ;ACT_INACT_CTL 0x27 0b00100111 controla los ejes que intervienen
+	LDI R26, 0b00001111	;Dato a transmitir ;ACT_INACT_CTL 
+				;en 0b00001111 (D3 en 1=ac coupled)(D2D1D0 = 111 enable en los tres ejes)
+	RCALL MULTIPLE_BYTE_WRITE
+;	RCALL DELAY
+
+	
+	LDI R25, 0x2C		;Direccion del registro a escribir ;BW_RATE 0x2C 0b00101100 Data rate and power mode control
+	LDI R26, 0b00000110	;Dato a transmitir ;BW_RATE
+				;(D4=0 sin Low Power)(Rate D3D2D1D0 = 0110 : BW=3.13Hz) 
+	RCALL MULTIPLE_BYTE_WRITE
+;	RCALL DELAY
+
+
+	LDI R25, 0x2F		;Direccion del registro a escribir ;INT_MAP 0x2F 0b00101111 Interrupt mapping control
+	LDI R26, 0b11110111	;Dato a transmitir ;INT_MAP
+				;solo la inactividad conectada al pin INT1, el resto conectadas al pin INT2
+	RCALL MULTIPLE_BYTE_WRITE
+;	RCALL DELAY
+
+
+	LDI R25, 0x2E		;Direccion del registro a escribir ;INT_MAP 0x2E 0b00101110 Interrupt enable control
+	LDI R26, 0b00001000	;Dato a transmitir ;INT_ENABLE
+				;se habilita solo la interrupcion por inactividad
+	RCALL MULTIPLE_BYTE_WRITE
+;	RCALL DELAY
+
+
+; Solo queda la duda de si en Reg 0x2D POWER_CTL hay que poner el (Measure)D3 en 1 para que anden las interrupciones o no
+; Por defecto esta en 0 al encenderse en modo standby
+
+	LDI R25, 0x2D	;Direccion del registro a escribir ;0x2D POWER_CTL 0b00101101 
+	LDI R26, 0b00001000	;Dato a transmitir ; MEASURE ON
+				;se pone en 1 el bit de Measure, por defecto se prende en modo standby, no queda claro si es necesario
+				;ponerlo en modo medicion para que anden las interrupciones
+	RCALL MULTIPLE_BYTE_WRITE
+;	RCALL DELAY
+
+
+*/
+	
+	
+	
+	}
     
     for(;;);	
 }
